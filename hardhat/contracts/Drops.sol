4e05b0de272e3010f40c8fceaceb980cc13b890c
Ezra Weller <ezra.weller@gmail.com>
2021-09-25 16:56:05 -0400

    first

---
+++ hardhat/contracts/Drops.sol
@@ -0,0 +1,183 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.5;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IWETH {
+    function deposit() external payable;
+    function transfer(address to, uint256 value) external returns (bool);
+}
+
+/**
+ * @title Drops
+ *
+ * Can pool any number of revenue splits into periodic "drops."
+ * Requires a trusted owner (DAO) to calculate shares and make new drops.
+ *
+ * Extends work by other teams:
+ * https://github.com/mirror-xyz/splits
+ * https://github.com/Uniswap/merkle-distributor
+ *
+ */
+contract Drops is Ownable {
+  //======== State ========
+  bytes32[] public dropRoots;
+  uint256 public unclaimedEth;
+  mapping(bytes32 => bool) internal claimed;
+  IWETH weth;
+
+  //======== Events ========
+  event Payment(string split, address from, uint256 amount);
+  event Drop(bytes32 dropRoot, uint256 dropTotal);
+  event Claim(
+    uint256 dropNumber, 
+    address recipient, 
+    uint256 amount,
+    bool wrapped
+  );
+
+  //======== Constructor ========
+  constructor(address wethAddress) {
+    weth = IWETH(wethAddress);
+  }
+
+  //======== External Functions ========
+  function pay(string calldata split, address from) external payable {
+    require(msg.value > 0, "Value was 0");
+    emit Payment(split, from, msg.value);
+  }
+
+  function drop(bytes32 dropRoot, uint256 dropAmount) external onlyOwner {
+    require(
+      address(this).balance - dropAmount >= unclaimedEth,
+      "Drop too large"
+    );
+    dropRoots.push(dropRoot);
+    unclaimedEth += dropAmount;
+    emit Drop(dropRoot, dropAmount);
+  }
+
+  function multiClaim(
+    uint256[] calldata dropNumbers,
+    address[] calldata recipients, 
+    uint256[] calldata amounts, 
+    bytes32[][] calldata proofs
+  ) external {
+    require(
+      recipients.length == amounts.length &&
+      amounts.length == dropNumbers.length &&
+      dropNumbers.length == proofs.length,
+      "Input array lengths mismatched"
+    );
+    for (uint256 i = 0; i < recipients.length; i++) {
+      claim(
+        dropNumbers[i],
+        recipients[i],
+        amounts[i],
+        proofs[i]
+      );
+    }
+  }
+  
+  //======== Public Functions ========
+  function claim(
+    uint256 dropNumber,
+    address recipient, 
+    uint256 amount,  
+    bytes32[] calldata proof
+  ) public {
+    require(dropRoots.length >= dropNumber, "Drop doesn't exist");
+    require(!isClaimed(dropNumber, recipient), "Already claimed");
+    claimed[getClaimHash(dropNumber, recipient)] = true;
+    require(
+      verifyProof(
+        proof, 
+        dropRoots[dropNumber], 
+        getLeaf(recipient, amount)
+      ),
+      "Invalid proof"
+    );
+    bool wrapped = transferETHOrWETH(recipient, amount);
+    emit Claim(dropNumber, recipient, amount, wrapped);
+  }
+
+  function isClaimed(
+    uint256 dropNumber, 
+    address recipient
+  ) public view returns (bool) {
+    return claimed[getClaimHash(dropNumber, recipient)];
+  }
+
+  //======== Private Functions ========
+  // Will attempt to transfer ETH, but will transfer WETH instead if it fails.
+  function transferETHOrWETH(
+    address to, 
+    uint256 value
+  ) private returns (bool didSucceed) {
+    // Try to transfer ETH to the given recipient.
+    didSucceed = attemptETHTransfer(to, value);
+    if (!didSucceed) {
+      // If the transfer fails, wrap and send as WETH, so that
+      // the auction is not impeded and the recipient still
+      // can claim ETH via the WETH contract (similar to escrow).
+      weth.deposit{value: value}();
+      weth.transfer(to, value);
+      // At this point, the recipient can unwrap WETH.
+    }
+    return !didSucceed;
+  }
+
+  function attemptETHTransfer(
+    address to, 
+    uint256 value
+  ) private returns (bool) {
+    // Here increase the gas limit a reasonable amount above the default, and try
+    // to send ETH to the recipient.
+    // NOTE: This might allow the recipient to attempt a limited reentrancy attack.
+    (bool success, ) = to.call{value: value, gas: 30000}("");
+    return success;
+  }
+
+  function getClaimHash(
+    uint256 dropNumber, 
+    address recipient
+  ) private pure returns (bytes32) {
+    return keccak256(abi.encodePacked(dropNumber, recipient));
+  }
+
+  function getLeaf(
+    address recipient, 
+    uint256 amount
+  ) private pure returns (bytes32) {
+    return keccak256(abi.encodePacked(recipient, amount));
+  }
+
+  // From https://github.com/protofire/zeppelin-solidity/blob/master/contracts/MerkleProof.sol
+  function verifyProof(
+    bytes32[] calldata proof,
+    bytes32 root,
+    bytes32 leaf
+  ) private pure returns (bool) {
+    bytes32 computedHash = leaf;
+
+    for (uint256 i = 0; i < proof.length; i++) {
+      bytes32 proofElement = proof[i];
+
+      if (computedHash <= proofElement) {
+        // Hash(current computed hash + current element of the proof)
+        computedHash = keccak256(
+          abi.encodePacked(computedHash, proofElement)
+        );
+      } else {
+        // Hash(current element of the proof + current computed hash)
+        computedHash = keccak256(
+          abi.encodePacked(proofElement, computedHash)
+        );
+      }
+    }
+
+    // Check if the computed hash (root) is equal to the provided root
+    return computedHash == root;
+  }
+}
---
+++ hardhat/hardhat.config.ts
@@ -0,0 +1,9 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+
+export default {
+  solidity: "0.8.5",
+};
